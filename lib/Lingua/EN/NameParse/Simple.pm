c7b0d4c025f42b36954f8a4713da1fcfdf29c64b
Hugh Esco <hesco@campaignfoundations.com>
2010-04-08 17:44:12 +0000

    Import of HESCO/Lingua-EN-NameParse-Simple-0.12 from CPAN.
    gitpan-cpan-distribution: Lingua-EN-NameParse-Simple
    gitpan-cpan-version:      0.12
    gitpan-cpan-path:         HESCO/Lingua-EN-NameParse-Simple-0.12.tar.gz
    gitpan-cpan-author:       HESCO
    gitpan-cpan-maturity:     released

--- lib/Lingua/EN/NameParse/Simple.pm
+++ lib/Lingua/EN/NameParse/Simple.pm
@@ -13,11 +13,11 @@ Lingua::EN::NameParse::Simple - Parse an English name into component parts
 
 =head1 VERSION
 
-Version 0.11
+Version 0.12
 
 =cut
 
-our $VERSION = '0.11';
+our $VERSION = '0.12';
 
 =head1 SYNOPSIS
 
